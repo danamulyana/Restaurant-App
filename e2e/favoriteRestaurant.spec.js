1a011542b2166d35e92e93c49717633d1dddd00c
Dana Mulyana <danabontot@gmail.com>
2023-01-19 11:29:59 +0700

    update e2e unlike

--- e2e/favoriteRestaurant.spec.js
+++ e2e/favoriteRestaurant.spec.js
@@ -8,20 +8,26 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
-Scenario('showing empty favorite restaurant', ({ I }) => {
+const nodataCondition = 'Resto favorit kosong. Letakkan satu, dengan mengklik tombol hati di halaman detail.';
+
+Scenario('showing empty favorite restaurants', ({ I }) => {
   I.seeElement('#catalog-data');
-  I.see('Resto favorit kosong. Letakkan satu, dengan mengklik tombol hati di halaman detail.', '.catalog__noData');
+  I.see(nodataCondition, '.catalog__noData');
 });
 
-Scenario('liking one restaurnat', async ({ I }) => {
-  I.see('Resto favorit kosong. Letakkan satu, dengan mengklik tombol hati di halaman detail.', '.catalog__noData');
+Scenario('liking and unliking one restaurant', async ({ I }) => {
+  // Like Restaurant
+  I.see(nodataCondition, '.catalog__noData');
 
   I.amOnPage('/');
+
   I.seeElement('article.catalog__card');
 
   const firstRestoCard = locate('.catalog__link').first();
   const firstRestoCardTitle = await I.grabTextFrom(firstRestoCard);
   I.click(firstRestoCard);
+
+  I.wait(2);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
@@ -31,4 +37,23 @@ Scenario('liking one restaurnat', async ({ I }) => {
 
   const likedCardTitle = await I.grabTextFrom('.catalog__link');
   assert.strictEqual(firstRestoCardTitle, likedCardTitle);
+
+  // unlike
+  I.wait(3);
+
+  I.click(firstRestoCard);
+
+  I.wait(3);
+
+  I.seeElement('#likeButton');
+
+  I.wait();
+
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#catalog-data');
+
+  const unFavoriteResto = await I.grabTextFrom('.catalog__noData');
+  assert.strictEqual(unFavoriteResto, nodataCondition);
 });
